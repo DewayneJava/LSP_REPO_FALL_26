7398f8290cf74038aaa8d24f24f55d11eac0974a
DewayneJava <stephensd@bxscience.edu>
2026-08-24 00:22:30 -0400

    Update HelloWorld.java

--- src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -3,7 +3,8 @@ package org.howard.edu.lsp.assignment
   /**
   *Dewayne Stephenson
   */
-
+public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
+}
